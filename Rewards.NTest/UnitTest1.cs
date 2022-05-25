2a34edb01c826a478084d30025397e535b7e5171
Sabarish Kumar <sabarish204@hotmail.com>
2022-05-25 23:19:34 +0530

    CalculateRewardPointsIsnotEqual test case added

--- Rewards.NTest/UnitTest1.cs
+++ Rewards.NTest/UnitTest1.cs
@@ -15,10 +15,16 @@ namespace Rewards.NTest
         [Test]
         public void CalculateRewardPoints()
         {
-
             Assert.AreEqual(90, bal.CalculateRewardPoints(120));
             Assert.AreEqual(110, bal.CalculateRewardPoints(130));
             Assert.AreEqual(410, bal.CalculateRewardPoints(280));
+        }
+
+        [Test]
+        public void CalculateRewardPointsIsnotEqual()
+        {
+            Assert.AreNotEqual(100, bal.CalculateRewardPoints(120));
+            
         }
     }
 }
